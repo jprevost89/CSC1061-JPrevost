954f69a00cd3042f1b4ea293c049678767cd51f7
jprevost89 <37850190+jprevost89@users.noreply.github.com>
2025-08-29 13:30:53 -0600

    added comments

--- HomeworkAcctPay/CommissionEmployee.java
+++ HomeworkAcctPay/CommissionEmployee.java
@@ -1,39 +1,39 @@
-class CommissionEmployee extends Employee {
+class CommissionEmployee extends Employee { // start class CommissionEmployee
 
    private double grossSales;
    private double commissionRate;
    
-   public CommissionEmployee(String firstName, String lastName, String socialSecurity, double grossSales, double commissionRate) {
+   public CommissionEmployee(String firstName, String lastName, String socialSecurity, double grossSales, double commissionRate) { // start Constructor CommissionEmployee
       super(firstName, lastName, socialSecurity);
       this.grossSales = grossSales;
       this.commissionRate = commissionRate;
-   }
+   } // end Constructor CommissionEmployee
    
-   public double getSales() {
+   public double getSales() { // start method getSales
       return grossSales;
-   }
+   } // end method getSales
    
-   public void setSales(double grossSales) {
+   public void setSales(double grossSales) { // start method setSales
       this.grossSales = grossSales;
-   }
+   } // end method setSales
    
-   public double getRate() {
+   public double getRate() { // start method getRate
       return grossSales;
-   }
+   } // end method getRate
    
-   public void setRate(double commissionRate) {
+   public void setRate(double commissionRate) { // start method setRate
       this.commissionRate = commissionRate;
-   }
+   } // end method setRate
    
    @Override
-   public double getPaymentAmount() {
+   public double getPaymentAmount() { // start method getPaymentAmount
       return grossSales * commissionRate;
-   }
+   } // end method getPaymentAmount
    
    @Override
-   public String toString() {
+   public String toString() { // start method toString
       return super.toString() +
          "\nPayment Amount: " + getPaymentAmount();
-   }
+   } // end method toString
 
-}
+} // end class CommissionEmployee
--- HomeworkAcctPay/SalariedEmployee.java
+++ HomeworkAcctPay/SalariedEmployee.java
@@ -1,29 +1,29 @@
-class SalariedEmployee extends Employee {
+class SalariedEmployee extends Employee { // start class SalariedEmployee
    
    private double weeklySalary;
    
-   public SalariedEmployee(String firstName, String lastName, String socialSecurity, double weeklySalary) {
+   public SalariedEmployee(String firstName, String lastName, String socialSecurity, double weeklySalary) { // start Constructor Salaried Employee
       super(firstName, lastName, socialSecurity);
       this.weeklySalary = weeklySalary;
-   }
+   } // end Constructor Salaried Employee
    
-   public double getSalary() {
+   public double getSalary() { // start method getSalary
       return weeklySalary;
-   }
+   } // end method getSalary
    
-   public void setSalary(double weeklySalary) {
+   public void setSalary(double weeklySalary) { // start method setSalary
       this.weeklySalary = weeklySalary;
-   }
+   } // end method setSalary
    
    @Override
-   public double getPaymentAmount() {
+   public double getPaymentAmount() { // start method getPaymentAmount
       return weeklySalary;
-   }
+   } // end method getPaymentAmount
    
    @Override
-   public String toString() {
+   public String toString() { // start method toString
       return super.toString() +
          "\nPayment Amount: " + weeklySalary;
-   }
+   } // end method toString
 
-}
+} // end class SalariedEmployee
